fe90c3693d87689cf43f5e28fd93a5185955a5b9
@vadim17 <vadim@genesort.com>
2019-11-04 23:10:54 +0200

    commit

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,6 @@
-import {combineReducers} from "redux";
+import { combineReducers } from "redux";
 import registerReducer from "../reducers/registerReducer";
 
 export default combineReducers({
-  auth: combineReducers({
-    register: registerReducer
-  })
+  register: registerReducer
 });
