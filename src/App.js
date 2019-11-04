f113eee21d1f35f6a940823b6af32aabc80c9fb2
@vadim17 <vadim@genesort.com>
2019-11-04 18:25:25 +0200

    style

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from "react";
 import style from "./App.module.scss";
 import PageLayout from "./components/pageLayout";
-import {Button} from "antd";
+import { Button } from "antd";
 
 class App extends Component {
   handleDoSomething = e => {
     console.log(e.target);
   };
-  
+
   render() {
     return (
       <div className={style["App"]}>
-        <header className="App-header" />
         <PageLayout>
-          <div className="add-new-item-wrapper">
-            <Button type="primary" onClick={this.handleDoSomething}>Do something</Button>
-          </div>
+          <header className="App-header">
+            <div className="add-new-item-wrapper">
+              <Button type="primary" onClick={this.handleDoSomething}>
+                Do something
+              </Button>
+            </div>
+          </header>
           <div className="main-page-content">
             <nav></nav>
             <article></article>
