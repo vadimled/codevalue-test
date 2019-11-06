908c98fa2f7da38ef8c0e241039c115ca61280d6
@vadim17 <vadim@genesort.com>
2019-11-06 16:02:07 +0200

    commit

--- src/components/productDetailes/productDetails.js
+++ src/components/productDetailes/productDetails.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import {Input} from "antd";
+import {Button, Input} from "antd";
 const { TextArea } = Input;
 
-function ProductDetails({nameValue, descriptionValue, priceValue}) {
+function ProductDetails({nameValue, descriptionValue, priceValue, submit}) {
+  
+  const isSubmitEnabled = () => {
+    return nameValue && descriptionValue && priceValue;
+  };
+  
+  
   return (
     <div className={style["product-details-wrapper"]}>
       <form className="evidence-form" onSubmit={submit}>
@@ -28,11 +34,24 @@ function ProductDetails({nameValue, descriptionValue, priceValue}) {
             value={descriptionValue}
             name={"product-description-textarea"}
             onChange={onChange}
+            required
           />
         </div>
+        <div className="form-item">
+          <label htmlFor={"price"}>Source</label>
+          <Input
+            id="product-price"
+            name="productPrice"
+            onChange={onChange}
+            className={"form-item-source"}
+            value={priceValue}
+            required
+          />{ "$"}
+        </div>
+  
         <div className="divider" />
         <Button type="primary" htmlType="submit" disabled={!isSubmitEnabled()}>
-          {actionModeText(mode)}
+          Save
         </Button>
       </form>
     </div>
