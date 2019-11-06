6e8e52b7c28ec0a680e56424b76a9193c6fcde89
@vadim17 <vadim@genesort.com>
2019-11-06 14:41:39 +0200

    commit

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import style from "./product.module.scss";
+import {Button} from "antd";
 
-function Product({data, handleDelete}) {
+function Product({data, handleDelete,handleEdit}) {
+  const {description, name} = data;
   return (
-    <li className={style["product-wrapper"]}>
-    
+    <li className={style["product-wrapper"]} onClick={handleEdit}>
+      <div className="image"/>
+      <div className="description-wrapper">
+        <div className="name">{name}</div>
+        <div className="description">{description}</div>
+      </div>
+      <Button type="primary"
+               onClick={handleDelete}
+              size="large">Delete</Button>
     </li>
   );
 }
